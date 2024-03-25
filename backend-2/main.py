0b73d3e73fb58f3f749e6a6850f846f0da8e5519
jenhoang1 <jenh@vt.edu>
2024-03-24 21:54:11 -0400

    now using timetable api

--- backend-2/main.py
+++ backend-2/main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv, find_dotenv
 import os
 import pprint
 from pymongo import MongoClient
+from vtt import get_crn, Semester
 
 load_dotenv(find_dotenv())
 password = os.environ.get("MONGODB_PWD")
@@ -24,5 +25,13 @@ def update_user_document(user_email, new_data):
     user_collection = course_db.users
     user_collection.update_one({"email": user_email}, {"$set": new_data})
     print(f"Updated user document with email: {user_email}")
-    
+
+def get_course_crn(term_year, semester, crn):
+    course = get_crn(term_year, semester, crn)
+    open = course.has_open_spots()
+    return [course._course_data, open]
+
+# course = get_crn('2024', Semester.SPRING, '21892')
+course = get_course_crn('2024', Semester.SPRING, '21892')
+print(course)
 # update_user_document("jenh@vt.edu", {"name": "Jen Hoang", "password": "password1234"})
