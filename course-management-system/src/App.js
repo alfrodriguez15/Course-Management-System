29165573dcb98535bc88ca87c1c42561fb8d3760
dpark26 <danielpark0416@gmail.com>
2024-03-28 21:52:48 -0400

    about us and some of services frontend

--- course-management-system/src/App.js
+++ course-management-system/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import { Route, Routes } from "react-router-dom"
-import Home from './pages/Home.js'
-import Login from './pages/Login.js'
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/Home.js';
+import Login from './pages/Login.js';
+import About from './pages/About.js';
+import Services from './pages/Services.js';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/aboutus" element={<h1>about us</h1>}></Route>
-      <Route path="/services" element={<h1>services</h1>}></Route>
+      <Route path="/aboutus" element={<About />}></Route>
+      <Route path="/services" element={<Services />}></Route>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/signup" element={<h1>signup</h1>}></Route>
     </Routes>
