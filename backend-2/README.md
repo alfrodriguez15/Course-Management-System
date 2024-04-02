79c6fef32bfc8f6c221cc7f11be146dfd1873f30
jenhoang1 <jenh@vt.edu>
2024-04-02 13:34:25 -0400

    trying to integrate python flask

---
+++ backend-2/README.md
@@ -0,0 +1,8 @@
+Steps to set up:
+1. Download and install MongoDB Community Server
+2. Download and install MongoDB Compas (GUI)
+3. On powershell, cd into the app's directory
+    - Commands:
+        - pipenv install flask
+        - pipenv install pymongo
+4. Switch to virtual environment interpreter on VSCode
--- course-management-system/src/pages/Login.js
+++ course-management-system/src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import Navbar from '../components/Navbar'
 import './Login.css'
 import Button from 'react-bootstrap/Button';
@@ -11,7 +12,14 @@ function Login() {
     const [password, setPassword] = useState('');
 
     function handleClickSign() {
-        navigate("/signin");
+        axios.post('http://localhost:5000/login', { username, password })
+        .then(response => {
+            console.log(response.data); // Log the response from the backend
+            // Optionally, you can handle the response here, such as redirecting the user if login is successful
+        })
+        .catch(error => {
+            console.error(error); // Log any errors that occur during the request
+        });
     }
 
     /**
