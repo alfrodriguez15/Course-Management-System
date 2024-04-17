17ccff81d7b1e8cf0a8b9d7ac50a35510a6db9e2
Jenny <jennynt@vt.edu>
2024-04-17 12:59:31 -0400

    avoiding merge conflicts, small changes

--- course-management-system/src/App.js
+++ course-management-system/src/App.js
@@ -10,6 +10,7 @@ import Student from './pages/student/Student.js';
 import Schedule from './pages/student/Schedule.js';
 import Analytics from './pages/Analytics.js';
 import Ratings from './pages/student/Ratings.js';
+import Professors from './pages/student/Professors.js';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
       <Route path="/signup" element={<h1>signup</h1>}></Route>
       <Route path="/analytics" element={<Analytics />}></Route>
       <Route path = "/ratings" element = {<Ratings />}></Route> 
+      <Route path = "/professors" element = {<Professors/>}></Route>
     </Routes>
   )
 }
---
+++ course-management-system/src/pages/student/Professors.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Sidebar from './components/Sidebar';
+
+export default function Professors() {
+    return (
+        <>
+            <Sidebar />
+        
+        </>
+    );
+}
